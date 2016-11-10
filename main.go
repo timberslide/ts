e90ebbe5f10ceef153f2efe7a6cee19623ddc1ce
Kris Foster <kris.foster@gmail.com>
2016-11-10 13:58:02 -0800

    timberslide client code moved into timberslide package

--- main.go
+++ main.go
@@ -1,33 +1,17 @@
 package main
 
 import (
-	"bufio"
-	"context"
 	"errors"
 	"flag"
 	"fmt"
-	"io/ioutil"
-	"net"
 	"os"
-	"os/user"
 
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
-	"google.golang.org/grpc/metadata"
-
-	"github.com/BurntSushi/toml"
 	"github.com/timberslide/gotimberslide"
 )
 
 var (
 	configFile = ".timberslide/config"
 )
-
-// Client contains our client configuration
-type Client struct {
-	Host  string
-	Token string
-}
 
 // usage is displayed if the user tries to do something invalid
 func usage() {
@@ -37,101 +21,10 @@ func usage() {
 	flag.PrintDefaults()
 }
 
-// newClient creates a new client from a default config file
-func newClient() (Client, error) {
-	var client Client
-
-	usr, err := user.Current()
-	if err != nil {
-		return client, err
-	}
-
-	rcLoc := fmt.Sprintf("%s/%s", usr.HomeDir, configFile)
-	b, err := ioutil.ReadFile(rcLoc)
-	if err != nil {
-		return client, fmt.Errorf("Could not read configuration at %s", rcLoc)
-	}
-	if _, err = toml.Decode(string(b), &client); err != nil {
-		return client, err
-	}
-
-	return client, nil
-}
-
-// send takes stdin and sends it to the topic
-func (c *Client) send(topic string) error {
-	fmt.Fprintf(os.Stderr, "ts: connecting...\n")
-	host, _, err := net.SplitHostPort(c.Host)
-	if err != nil {
-		return err
-	}
-	creds := credentials.NewClientTLSFromCert(nil, host)
-	conn, err := grpc.Dial(c.Host, grpc.WithTransportCredentials(creds), grpc.WithBlock())
-	if err != nil {
-		return err
-	}
-	defer conn.Close()
-
-	client := ts.NewIngestClient(conn)
-	md := metadata.Pairs("authorization", fmt.Sprintf("Bearer %s", c.Token))
-	ctx := metadata.NewContext(context.Background(), md)
-	stream, err := client.StreamEvents(ctx)
-	if err != nil {
-		return err
-	}
-
-	msg := &ts.Event{Topic: topic}
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		msg.Message = scanner.Text()
-		// TODO make printing to stdout optional
-		fmt.Println(msg.Message)
-		stream.Send(msg)
-	}
-	if err := scanner.Err(); err != nil {
-		return err
-	}
-	// We finished normally, so...
-	// set the done flag and block until we hear something from the server
-	stream.Send(&ts.Event{Topic: topic, Done: true})
-	fmt.Fprintf(os.Stderr, "ts: done, waiting for server...\n")
-	stream.Recv()
-	stream.CloseSend()
-
-	return nil
-}
-
-// get receives the stream from the topic and writes it to stdout
-func (c *Client) get(topic string) error {
-	host, _, err := net.SplitHostPort(c.Host)
-	if err != nil {
-		return err
-	}
-	creds := credentials.NewClientTLSFromCert(nil, host)
-	conn, err := grpc.Dial(c.Host, grpc.WithTransportCredentials(creds))
-	if err != nil {
-		return err
-	}
-	client := ts.NewStreamerClient(conn)
-	md := metadata.Pairs("authorization", fmt.Sprintf("Bearer %s", c.Token))
-	ctx := metadata.NewContext(context.Background(), md)
-	stream, err := client.GetStream(ctx, &ts.Topic{Topic: topic})
-	if err != nil {
-		return err
-	}
-	for {
-		event, err := stream.Recv()
-		if err != nil {
-			return err
-		}
-		fmt.Println(event.Message)
-	}
-}
-
 func main() {
 	var err error
 
-	client, err := newClient()
+	client, err := ts.NewClient(configFile)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
@@ -148,9 +41,9 @@ func main() {
 
 	switch action {
 	case "send":
-		err = client.send(topic)
+		err = client.Send(topic)
 	case "get":
-		err = client.get(topic)
+		err = client.Get(topic)
 	default:
 		err = errors.New(fmt.Sprintln("Unknown command:", flag.Arg(0)))
 	}
