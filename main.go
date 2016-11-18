60dc95b94cbe67681d91c2f49c543f24f66cf4f6
Kris Foster <kris.foster@gmail.com>
2016-11-18 15:22:50 -0800

    expose a flag to start streaming from oldest message

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"flag"
 	"fmt"
 	"os"
@@ -13,10 +12,16 @@ var (
 	configFile = ".timberslide/config"
 )
 
+const (
+	ErrConfig = iota // ErrConfig is returned if we have a proble with the config file
+	ErrUsage         // ErrUsage is returned if we exit because we displayed the usage
+	ErrServer        // ErrServer is returned for any error because of the server
+)
+
 // usage is displayed if the user tries to do something invalid
 func usage() {
 	fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
-	fmt.Fprintf(os.Stderr, "%s <action> [<topic>]\n", os.Args[0])
+	fmt.Fprintf(os.Stderr, "%s <action> [options...] <topic>\n", os.Args[0])
 	fmt.Fprintf(os.Stderr, "Actions: send, get\n")
 	flag.PrintDefaults()
 }
@@ -24,31 +29,45 @@ func usage() {
 func main() {
 	var err error
 
+	// Configure the Timberslide client
 	client, err := ts.NewClient(configFile)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+		os.Exit(ErrConfig)
 	}
 	flag.Usage = usage
-	flag.Parse()
-	// We expect 2 arguments: an action and a topic
-	if flag.Arg(1) == "" {
-		usage()
-		os.Exit(1)
-	}
-	action := flag.Arg(0)
-	topic := flag.Arg(1)
 
-	switch action {
+	if len(os.Args) < 3 {
+		usage()
+		os.Exit(ErrUsage)
+	}
+
+	// Set up our command line flags
+	sendCmd := flag.NewFlagSet("send", flag.ExitOnError)
+
+	getCmd := flag.NewFlagSet("get", flag.ExitOnError)
+	var all bool
+	getCmd.BoolVar(&all, "all", false, "Begin from the oldest message")
+
+	topic := os.Args[len(os.Args)-1]
+
+	switch os.Args[1] {
 	case "send":
+		sendCmd.Parse(os.Args[2:])
 		err = client.Send(topic)
 	case "get":
-		err = client.Get(topic)
+		getCmd.Parse(os.Args[2:])
+		position := ts.PositionNewest
+		if all {
+			position = ts.PositionOldest
+		}
+		err = client.Get(topic, position)
 	default:
-		err = errors.New(fmt.Sprintln("Unknown command:", flag.Arg(0)))
+		fmt.Fprintf(os.Stderr, "%s is not a valid command. Valid commands are `send` and `get`.\n", os.Args[1])
+		os.Exit(ErrUsage)
 	}
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Error:", err)
-		os.Exit(1)
+		os.Exit(ErrServer)
 	}
 }
